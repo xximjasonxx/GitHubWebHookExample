9f39cb0b2a3792a2495f213efcb25ef96f52cf55
Jason Farrell <imjason@gmail.com>
2023-01-25 10:32:04 -0500

    Updated the working directory for the terraform run calls

--- HttpGitHubWorkflowExecute.cs
+++ HttpGitHubWorkflowExecute.cs
@@ -27,31 +27,31 @@ namespace GithubHook
             string app_name,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"GitHub Token: {_configuration["GitHubToken"]}");
 
             var creds = new Credentials(_configuration["GitHubToken"]);
             var client = new GitHubClient(new ProductHeaderValue("xximjasonxx"));
             client.Credentials = creds;
 
+            var dispatchEvent = new CreateWorkflowDispatch("main");
+            dispatchEvent.Inputs = new Dictionary<string, object>();
+            dispatchEvent.Inputs.Add("app_name", app_name);
+
+            var app_roles = new List<AppRole>()
+            {
+                new AppRole() { display_name = "Admin", role_name = "admin" },
+                new AppRole() { display_name = "Content Writer", role_name = "content.write" }
+            };
+            
+            dispatchEvent.Inputs.Add("app_roles", JsonConvert.SerializeObject(app_roles));
+
             try
             {
-                var dispatchEvent = new CreateWorkflowDispatch("main");
-                dispatchEvent.Inputs = new Dictionary<string, object>();
-                dispatchEvent.Inputs.Add("app_name", app_name);
-
-                var app_roles = new List<AppRole>()
-                {
-                    new AppRole() { display_name = "Admin", role_name = "admin" },
-                    new AppRole() { display_name = "Content Writer", role_name = "content.write" }
-                };
-                
-                dispatchEvent.Inputs.Add("app_roles", JsonConvert.SerializeObject(app_roles));
-
                 await client.Actions.Workflows.CreateDispatch("xximjasonxx", "GitHubWebHookExample", "blank.yml", dispatchEvent);
             }
             catch (Exception ex)
             {
-                log.LogError(ex, "Error executing workflow");
+                return new OkObjectResult(ex.Message);
             }
 
             return new OkObjectResult("Hello World");
